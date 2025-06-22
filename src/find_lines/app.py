7b663a1a344bd337bd197ad269a3fdea0bc4c1e2
David Fokkema <davidfokkema@icloud.com>
2025-06-22 11:38:46 +0200

    Don't show all columns

--- src/find_lines/app.py
+++ src/find_lines/app.py
@@ -10,6 +10,29 @@ app = typer.Typer()
 
 
 class FindLinesApp(App[None]):
+    _selected_columns = {
+        "element": True,
+        "sp_num": True,
+        "obs_wl(nm)": True,
+        "unc_obs_wl": False,
+        "ritz_wl_vac(nm)": True,
+        "unc_ritz_wl": False,
+        "intens": True,
+        "Aki(s^-1)": False,
+        "Acc": False,
+        "Ei(eV)": True,
+        "Ek(eV)": True,
+        "conf_i": True,
+        "term_i": False,
+        "J_i": False,
+        "conf_k": True,
+        "term_k": False,
+        "J_k": False,
+        "Type": False,
+        "tp_ref": False,
+        "line_ref": False,
+    }
+
     def __init__(self, path: Path, *args: Any, **kwargs: dict[str, Any]) -> None:
         super().__init__(*args, **kwargs)
         self.db_path = path
@@ -20,36 +43,21 @@ class FindLinesApp(App[None]):
         yield DataTable()
 
     def on_mount(self):
-        df = self.read_data_file()
+        self._data = self.read_data_file()
+        self.fill_table()
+
+    def fill_table(self):
         table = self.query_one(DataTable)
         table.cursor_type = "row"
-        columns = [
-            "element",
-            "sp_num",
-            "obs_wl(nm)",
-            "unc_obs_wl",
-            "ritz_wl_vac(nm)",
-            "unc_ritz_wl",
-            "intens",
-            "Aki(s^-1)",
-            "Acc",
-            "Ei(eV)",
-            "Ek(eV)",
-            "conf_i",
-            "term_i",
-            "J_i",
-            "conf_k",
-            "term_k",
-            "J_k",
-            "Type",
-            "tp_ref",
-            "line_ref",
-        ]
+        columns = [k for k, v in self._selected_columns.items() if v is True]
         table.add_columns(*columns)
         table.add_rows(
-            (("" if pd.isna(x) else x for x in r) for _, r in df[columns].iterrows())
+            (
+                ("" if pd.isna(x) else x for x in r)
+                for _, r in self._data[columns].iterrows()
+            )
         )
-        self.notify(f"Added {len(df)} rows.")
+        self.notify(f"Added {len(self._data)} rows.")
 
     def read_data_file(self):
         rows_to_skip = [
